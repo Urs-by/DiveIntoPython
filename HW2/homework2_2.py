b046eac0a31dea813768c66aff457d9e7a099cb0
Artur.by <urs.by@iclood.com>
2023-07-19 21:10:11 +0300

    Task 2 from hw2_to_hex solved

---
+++ HW2/homework2_2.py
@@ -0,0 +1,19 @@
+# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
+num_dec = 1000
+calculated = num_dec
+HEX_DIV = 16
+
+hex_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E"]
+res = ''
+while calculated > 0:
+
+    remainder = calculated % HEX_DIV
+    res += str(hex_list[remainder-1])
+    calculated = calculated // HEX_DIV
+
+res = res[::-1]
+print(res, hex(num_dec))
+
+
